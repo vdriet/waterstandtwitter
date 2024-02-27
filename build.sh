2da96318d5ee94f72101d0b4f71fde056a78eb54
Peter <peter@kokosnoot>
2024-02-27 20:13:24 +0100

    build met update requirements

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 grep TWITTER ../setenvvars.sh | awk -F ' ' '{print $2}' > env.list
+pip install -r requirements.txt
+pip list --outdated
 pylint *.py
 docker build --tag waterstandtwitter .
