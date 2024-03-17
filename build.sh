31d47d69cf6ff410dcdf28910ce3b96bd110855a
Peter <peter@kokosnoot>
2024-03-17 21:09:00 +0100

    Waterstand als module

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-grep TWITTER ../setenvvars.sh | awk -F ' ' '{print $2}' > env.list
-pip install -r requirements.txt
+pip install --no-cache-dir --index-url https://test.pypi.org/simple/ --extra-index-url=https://pypi.org/simple/ -r requirements.txt
 pip list --outdated
 pylint *.py
 docker build --tag waterstandtwitter .
