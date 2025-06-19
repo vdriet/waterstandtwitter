bd789e6e1d3755f8d0eb11934dea3a5a50c35f9e
Peter <peter@chromebook>
2025-06-19 21:44:42 +0200

    IT-307 testen op met combi goed en fout

--- tests/test_waterstandtwitter.py
+++ tests/test_waterstandtwitter.py
@@ -6,33 +6,56 @@ import waterstandtwitter
 
 class TestWaterstandTwitter(unittest.TestCase):
 
-  @patch('waterstand.haalwaterstand')
-  @patch('waterstandtwitter.tweetbericht')
-  @patch('waterstandtwitter.twitterwaterstand')
-  def test_main_nodata(self, mock_twitterwaterstand, mock_tweetbericht, mock_haalwaterstand):
-    mock_twitterwaterstand.return_value = ''
-    mock_tweetbericht.return_value = ''
-    mock_haalwaterstand.return_value = {'resultaat': 'NOK', 'tekst': 'Foutmelding'}
-
-    waterstandtwitter.main()
-
-    assert mock_haalwaterstand.called
-    assert mock_tweetbericht.called
-    assert mock_twitterwaterstand.called == False
-
-  @patch('waterstand.haalwaterstand')
+  @patch('waterstand.haalwaterstand',
+         side_effect=[{'resultaat': 'OK', 'tijd': '', 'nu': '', 'morgen': ''},
+                      {'resultaat': 'OK', 'tijd': '', 'nu': '', 'morgen': ''},
+                      {'resultaat': 'OK', 'tijd': '', 'nu': '', 'morgen': ''},
+                      ])
   @patch('waterstandtwitter.tweetbericht')
   @patch('waterstandtwitter.twitterwaterstand')
   def test_main_data(self, mock_twitterwaterstand, mock_tweetbericht, mock_haalwaterstand):
     mock_twitterwaterstand.return_value = ''
     mock_tweetbericht.return_value = ''
-    mock_haalwaterstand.return_value = {'resultaat': 'OK', 'tijd': '', 'nu': '', 'morgen': ''}
 
     waterstandtwitter.main()
 
-    assert mock_haalwaterstand.called
-    assert mock_tweetbericht.called == False
-    assert mock_twitterwaterstand.called
+    assert mock_haalwaterstand.call_count == 3
+    assert mock_tweetbericht.call_count == 0
+    assert mock_twitterwaterstand.call_count == 3
+
+  @patch('waterstand.haalwaterstand',
+         side_effect=[{'resultaat': 'NOK', 'tekst': 'Foutmelding'},
+                      {'resultaat': 'NOK', 'tekst': 'Foutmelding'},
+                      {'resultaat': 'NOK', 'tekst': 'Foutmelding'},
+                      ])
+  @patch('waterstandtwitter.tweetbericht')
+  @patch('waterstandtwitter.twitterwaterstand')
+  def test_main_nodata(self, mock_twitterwaterstand, mock_tweetbericht, mock_haalwaterstand):
+    mock_twitterwaterstand.return_value = ''
+    mock_tweetbericht.return_value = ''
+
+    waterstandtwitter.main()
+
+    assert mock_haalwaterstand.call_count == 3
+    assert mock_tweetbericht.call_count == 3
+    assert mock_twitterwaterstand.called == False
+
+  @patch('waterstand.haalwaterstand',
+         side_effect=[{'resultaat': 'NOK', 'tekst': 'Foutmelding'},
+                      {'resultaat': 'OK', 'tijd': '', 'nu': '', 'morgen': ''},
+                      {'resultaat': 'OK', 'tijd': '', 'nu': '', 'morgen': ''},
+                      ])
+  @patch('waterstandtwitter.tweetbericht')
+  @patch('waterstandtwitter.twitterwaterstand')
+  def test_main_somedata(self, mock_twitterwaterstand, mock_tweetbericht, mock_haalwaterstand):
+    mock_twitterwaterstand.return_value = ''
+    mock_tweetbericht.return_value = ''
+
+    waterstandtwitter.main()
+
+    assert mock_haalwaterstand.call_count == 3
+    assert mock_tweetbericht.call_count == 1
+    assert mock_twitterwaterstand.call_count == 2
 
   @patch('waterstandtwitter.tweetbericht')
   def test_twitterwaterstand_nokey(self, mock_tweetbericht):
